0cf9bc0c069e12f3b9f4c589270d9b4900606e2f
natanboanafina <boanafina.natan@gmail.com>
2022-09-08 18:29:08 -0300

    Project structure

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+	fetchData();
+});
+
+async function fetchData() {
+	// const { data } = await axios.get('https://swapi.dev/api/', {});
+	// console.table(data);
+}
