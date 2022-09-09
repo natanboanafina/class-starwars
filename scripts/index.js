85854f06a5192bd32c4bd3ecb940a778853b49c3
natanboanafina <boanafina.natan@gmail.com>
2022-09-08 22:19:36 -0300

    Some styles and some functionalities added

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,23 @@
-window.addEventListener('load', () => {
-	fetchData();
+const char = document.querySelector('#characters');
+const vehicle = document.querySelector('#vehicles');
+const results = document.querySelector('#results');
+
+char.addEventListener('click', (e) => {
+	const value = e.target.value;
+
+	try {
+		fetchData(name);
+		if (value === 'Characters') {
+			fetchData('people');
+		}
+	} catch (err) {
+		throw new Error(err);
+	}
 });
 
-async function fetchData() {
-	// const { data } = await axios.get('https://swapi.dev/api/', {});
-	// console.table(data);
+async function fetchData(name) {
+	const { data } = await axios.get(`https://swapi.dev/api/${name}`, {
+		headers: { 'Content-Type': 'application/json' },
+	});
+	console.table(data);
 }
